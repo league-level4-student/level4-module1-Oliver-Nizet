5487e3a00934b60c4a54b0ad953de5e370793944
11000783 <josephaguilar2022@gmail.com>
2018-11-20 18:29:39 -0800

    cloned

--- src/_01_Custom_ArrayList/ArrayList.java
+++ src/_01_Custom_ArrayList/ArrayList.java
@@ -4,16 +4,27 @@ package _01_Custom_ArrayList;
 
 public class ArrayList <T>{
 	
+	private T[] list;
+	
 	public ArrayList() {
+		list = (T[]) new Object[5];
 	}
 	
 	public T get(int loc) throws IndexOutOfBoundsException {
 		
-		return null;
+		return list[loc];
 	}
 	
 	public void add(T val) {
-		
+		T[] temp = (T[]) new Object[list.length+1];
+		for (int i = 0; i < temp.length; i++) {
+			if(i < temp.length-1) {
+				temp[i] = list [i];
+			} else {
+				temp[i] = val;
+			}
+		}
+		list = temp;
 	}
 	
 	public void insert(int loc, T val) throws IndexOutOfBoundsException {
